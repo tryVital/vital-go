eb8ec5a4e3e936114742bccd5a9b93aebff7f99e
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-09 13:58:53 +0000

    Release v1.1.266

--- core/request_option.go
+++ core/request_option.go
@@ -57,7 +57,7 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/tryVital/vital-go")
-	headers.Set("X-Fern-SDK-Version", "v1.1.265")
+	headers.Set("X-Fern-SDK-Version", "v1.1.266")
 	return headers
 }
 
