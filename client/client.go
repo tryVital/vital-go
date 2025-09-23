b65d06bc790ba870e91ea6fe795b98f858630e1a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-09-23 16:52:09 +0000

    Release v1.1.401

--- client/client.go
+++ client/client.go
@@ -35,24 +35,24 @@ type Client struct {
 	caller  *core.Caller
 	header  http.Header
 
+	User              *user.Client
+	Link              *link.Client
+	Sleep             *sleep.Client
+	Workouts          *workouts.Client
+	Vitals            *vitals.Client
 	Electrocardiogram *electrocardiogram.Client
 	SleepCycle        *sleepcycle.Client
 	Profile           *profile.Client
 	Devices           *devices.Client
 	Activity          *activity.Client
-	Workouts          *workouts.Client
-	Sleep             *sleep.Client
 	Body              *body.Client
 	Meal              *meal.Client
 	MenstrualCycle    *menstrualcycle.Client
-	Link              *link.Client
-	User              *user.Client
-	LabTests          *labtests.Client
 	Providers         *providers.Client
-	Vitals            *vitals.Client
+	LabTests          *labtests.Client
+	Team              *team.Client
 	Testkit           *testkit.Client
 	Order             *order.Client
-	Team              *team.Client
 	Introspect        *introspect.Client
 	Insurance         *insurance.Client
 	Payor             *payor.Client
@@ -70,24 +70,24 @@ func NewClient(opts ...option.RequestOption) *Client {
 			},
 		),
 		header:            options.ToHeader(),
+		User:              user.NewClient(opts...),
+		Link:              link.NewClient(opts...),
+		Sleep:             sleep.NewClient(opts...),
+		Workouts:          workouts.NewClient(opts...),
+		Vitals:            vitals.NewClient(opts...),
 		Electrocardiogram: electrocardiogram.NewClient(opts...),
 		SleepCycle:        sleepcycle.NewClient(opts...),
 		Profile:           profile.NewClient(opts...),
 		Devices:           devices.NewClient(opts...),
 		Activity:          activity.NewClient(opts...),
-		Workouts:          workouts.NewClient(opts...),
-		Sleep:             sleep.NewClient(opts...),
 		Body:              body.NewClient(opts...),
 		Meal:              meal.NewClient(opts...),
 		MenstrualCycle:    menstrualcycle.NewClient(opts...),
-		Link:              link.NewClient(opts...),
-		User:              user.NewClient(opts...),
-		LabTests:          labtests.NewClient(opts...),
 		Providers:         providers.NewClient(opts...),
-		Vitals:            vitals.NewClient(opts...),
+		LabTests:          labtests.NewClient(opts...),
+		Team:              team.NewClient(opts...),
 		Testkit:           testkit.NewClient(opts...),
 		Order:             order.NewClient(opts...),
-		Team:              team.NewClient(opts...),
 		Introspect:        introspect.NewClient(opts...),
 		Insurance:         insurance.NewClient(opts...),
 		Payor:             payor.NewClient(opts...),
