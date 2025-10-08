ca6c6073778aa7bd29452f90862e04893ae45401
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-08 11:05:24 +0000

    Release v1.1.425

--- client/client.go
+++ client/client.go
@@ -35,8 +35,6 @@ type Client struct {
 	caller  *core.Caller
 	header  http.Header
 
-	Providers         *providers.Client
-	User              *user.Client
 	Link              *link.Client
 	Electrocardiogram *electrocardiogram.Client
 	SleepCycle        *sleepcycle.Client
@@ -49,7 +47,9 @@ type Client struct {
 	Meal              *meal.Client
 	MenstrualCycle    *menstrualcycle.Client
 	Vitals            *vitals.Client
+	User              *user.Client
 	Team              *team.Client
+	Providers         *providers.Client
 	Introspect        *introspect.Client
 	LabTests          *labtests.Client
 	Testkit           *testkit.Client
@@ -70,8 +70,6 @@ func NewClient(opts ...option.RequestOption) *Client {
 			},
 		),
 		header:            options.ToHeader(),
-		Providers:         providers.NewClient(opts...),
-		User:              user.NewClient(opts...),
 		Link:              link.NewClient(opts...),
 		Electrocardiogram: electrocardiogram.NewClient(opts...),
 		SleepCycle:        sleepcycle.NewClient(opts...),
@@ -84,7 +82,9 @@ func NewClient(opts ...option.RequestOption) *Client {
 		Meal:              meal.NewClient(opts...),
 		MenstrualCycle:    menstrualcycle.NewClient(opts...),
 		Vitals:            vitals.NewClient(opts...),
+		User:              user.NewClient(opts...),
 		Team:              team.NewClient(opts...),
+		Providers:         providers.NewClient(opts...),
 		Introspect:        introspect.NewClient(opts...),
 		LabTests:          labtests.NewClient(opts...),
 		Testkit:           testkit.NewClient(opts...),
